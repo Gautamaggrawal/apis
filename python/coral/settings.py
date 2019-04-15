fcf138300069e4ed4f804ff05af51b07a08ee391
Gautam <gautamaggrawalsd@yahoo.in>
2019-04-15 18:33:51 +0530

    added gif

--- python/coral/settings.py
+++ python/coral/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o+jiok2+bi6f9z%wnhpu)$vf!s^8yn-r09fncky64hgrazbnc7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
